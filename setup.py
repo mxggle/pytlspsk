76c7e59ab536110813ac9adbb7621bff35e05a15
Jignesh Vasoya <jigneshvasoya@gmail.com>
2017-08-09 17:43:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ sslpsk = Extension(
 setup(
   name = 'pytlspsk',
   packages = ['pytlspsk'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'TLS-PSK wrapper for Socket',
   author = 'Jignesh Vasoya',
   author_email = 'jigneshvasoya@gmail.com',
   url = 'https://github.com/jigneshvasoya/pytlspsk', # use the URL to the github repo
-  download_url = 'https://github.com/jigneshvasoya/pytlspsk/tarball/0.2', # I'll explain this in a second
+  download_url = 'https://github.com/jigneshvasoya/pytlspsk/tarball/0.3', # I'll explain this in a second
   keywords = ['OpenSSL', 'TLS-PSK', 'tlspsk', 'security', 'ssl socket'], # arbitrary keywords
   classifiers = [
-      'Development Status :: 1 - Planning',
+      'Development Status :: 2 - Alpha',
       'Intended Audience :: Developers',
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python',
